487a7d6216ad8d7f3c703537d3412bd277199fcd
warlock <lvelezsantos@gmail.com>
2013-09-29 22:20:06 -0500

    Agregada action para eliminar datos definitivamente de la base de datos, solo disponible para el superusuario

--- logicaldelete/admin.py
+++ logicaldelete/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin, messages
+from django.contrib.admin import helpers
 from django.contrib.admin.filters import SimpleListFilter
+from django.contrib.admin.util import model_ngettext, get_deleted_objects
+from django.core.exceptions import PermissionDenied
+from django.db import router
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.utils.encoding import force_unicode
 from django.utils.translation import ugettext_lazy
-from datetime import datetime
+from django.utils.translation import ugettext as _
+
+
 
 class ActiveListFilter(SimpleListFilter):
     title = 'Borrados'
@@ -36,6 +45,8 @@ class LogicalModelAdmin(admin.ModelAdmin):
     #list_display = ("id", "active")
     #list_display_filter = ("active",)
 
+    delete_selected_complete_confirmation = None
+
     def __init__(self, *args, **kwargs):
         super(LogicalModelAdmin, self).__init__(*args, **kwargs)
 
@@ -52,7 +63,15 @@ class LogicalModelAdmin(admin.ModelAdmin):
     def get_actions(self, request):
         actions = super(LogicalModelAdmin, self).get_actions(request)
 
-        actions['undelete_selected'] = self.get_action('undelete_selected')
+        new_actions = {
+            'undelete_selected': self.get_action('undelete_selected'),
+        }
+
+        actions.update(new_actions)
+
+        if request.user.is_superuser:
+            actions.update({'delete_complete': self.get_action('delete_complete')})
+
 
         return actions
 
@@ -71,3 +90,82 @@ class LogicalModelAdmin(admin.ModelAdmin):
 
         queryset.update(date_removed=None)
     undelete_selected.short_description = ugettext_lazy("Restaurar  %(verbose_name_plural)s seleccionado/s")
+
+
+
+    def delete_complete(self, request, queryset):
+        """
+        Default action which deletes the selected objects.
+
+        This action first displays a confirmation page whichs shows all the
+        deleteable objects, or, if the user has no permission one of the related
+        childs (foreignkeys), a "permission denied" message.
+
+        Next, it delets all selected objects and redirects back to the change list.
+        """
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        # Check that the user has delete permission for the actual model
+        if not self.has_delete_permission(request):
+            raise PermissionDenied
+
+        if not request.user.is_superuser:
+            return PermissionDenied
+
+        using = router.db_for_write(self.model)
+
+        # Populate deletable_objects, a data structure of all related objects that
+        # will also be deleted.
+        deletable_objects, perms_needed, protected = get_deleted_objects(
+            queryset, opts, request.user, self.admin_site, using)
+
+        # The user has already confirmed the deletion.
+        # Do the deletion and return a None to display the change list view again.
+        if request.POST.get('post'):
+
+            if perms_needed:
+                raise PermissionDenied
+
+            n = queryset.count()
+            if n:
+                for obj in queryset:
+                    obj_display = force_unicode(obj)
+                    self.log_deletion(request, obj, obj_display)
+                queryset.delete_complete()
+                self.message_user(request, _("Successfully deleted %(count)d %(items)s.") % {
+                    "count": n, "items": model_ngettext(self.opts, n)
+                })
+            # Return None to display the change list page again.
+            return None
+
+        if len(queryset) == 1:
+            objects_name = force_unicode(opts.verbose_name)
+        else:
+            objects_name = force_unicode(opts.verbose_name_plural)
+
+        if perms_needed or protected:
+            title = _("Cannot delete %(name)s") % {"name": objects_name}
+        else:
+            title = _("Are you sure?")
+
+        context = {
+            "title": title,
+            "objects_name": objects_name,
+            "deletable_objects": [deletable_objects],
+            'queryset': queryset,
+            "perms_lacking": perms_needed,
+            "protected": protected,
+            "opts": opts,
+            "app_label": app_label,
+            'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
+        }
+
+        # Display the confirmation page
+        return TemplateResponse(request, self.delete_selected_complete_confirmation or [
+            "admin/%s/%s/delete_selected_complete_confirmation.html" % (app_label, opts.object_name.lower()),
+            "admin/%s/delete_selected_complete_confirmation.html" % app_label,
+            "admin/delete_selected_complete_confirmation.html"
+        ], context, current_app=self.admin_site.name)
+
+    delete_complete.short_description = ugettext_lazy("Elimina completamente lo(a)s %(verbose_name_plural)s seleccionado(a)/s")
